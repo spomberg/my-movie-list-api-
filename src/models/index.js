f620c58cfa5bf35a11d14185f9524da9f23f9465
Marcos Spomberg <marcosspomberg@gmail.com>
2022-07-12 20:30:18 -0400

    Create seeding script

--- src/models/index.js
+++ src/models/index.js
@@ -1,2 +1,26 @@
 module.exports.Token = require('./token.model');
 module.exports.User = require('./user.model');
+module.exports.List = require('./list.model');
+
+const path = require('path');
+const { Seeder } = require('mongo-seeding');
+
+const config = {
+  database: {
+    name: 'my-movie-list',
+  },
+  dropDatabase: true,
+};
+const seeder = new Seeder(config);
+const collections = seeder.readCollectionsFromPath(path.resolve('./example/data'), {
+  transformers: [Seeder.Transformers.replaceDocumentIdWithUnderscoreId],
+});
+
+seeder
+  .import(collections)
+  .then(() => {
+    console.log('Success');
+  })
+  .catch((err) => {
+    console.log('Error', err);
+  });
